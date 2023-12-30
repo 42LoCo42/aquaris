0996545e2292dd2ebe9cc66999610e4ad8756424
Eleonora <leonsch@protonmail.com>
2023-12-30 18:43:17 +0100

    template: use aquaris from flake.lock

--- example/secrets/secrets.nix
+++ example/secrets/secrets.nix
@@ -1,4 +1,3 @@
-# NOTE: you should use github:42loco42/aquaris/<commit>
-# this is just for easier debugging!
+# NOTE: the template has a real aquaris import, this is just for easier debugging!
 let aquaris = builtins.getFlake "path:/home/leonsch/dev/nix/aquaris"; in
 aquaris.lib.secretsHelper ./..
--- template/secrets/secrets.nix
+++ template/secrets/secrets.nix
@@ -1,2 +1,6 @@
-let aquaris = builtins.getFlake "github:42loco42/aquaris/b15ac3c"; in
+let
+  f = builtins.fromJSON (builtins.readFile ../flake.lock);
+  n = f.nodes.${f.nodes.${f.root}.inputs.aquaris}.locked;
+  aquaris = builtins.getFlake "${n.type}:${n.owner}/${n.repo}/${n.rev}";
+in
 aquaris.lib.secretsHelper ./..
