189285d38a806288debff3907dc3bce38b23b699
Leon Schumacher <leonsch@protonmail.com>
2024-01-08 22:13:07 +0100

    Update all secrets (I lost the keys lmao)

--- default.nix
+++ default.nix
@@ -6,14 +6,14 @@ let
 
     guy = {
       name = "justaguy"; # override actual user name
-      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP9fBvv8AWOYsItzYlomBJ41lGHwhV0cNtlyADn0zdP4";
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF7FppRoKRh+rTSnxFHodYmZ6lVEa4UWN7c0Sgy+trgl";
     };
   };
 
   machines = {
     castor = {
       id = "10ec6fa7b2fdeea772a40b31658fead8";
-      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEFR6KcY2CciSPuye3+OC3rj44adWz/1ZvqGMwpG+tlf";
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH8ebcoYSzs9koGhq9KtIqwgcJYj0siYdYv6hVUT/S/G";
       admins = { inherit (users) leonsch; }; # admins get sudo permissions & encryption access to machine secrets
       users = { inherit (users) guy; };
     };
@@ -21,7 +21,7 @@ let
     pollux = {
       # system = "aarch64-linux"; # optional, x86_64-linux is default
       id = "e755aeadc6c3f08afd03cf71658c2190";
-      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID0GHFF8wVFr5CGLMxyUSf/t4B2vaRor1IGVlrOh8I8y";
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGosXDmm4fVV28nlworyjrUxNLzqMVDqbkXGipM7ls+B";
       admins = { inherit (users) guy; };
     };
   };
