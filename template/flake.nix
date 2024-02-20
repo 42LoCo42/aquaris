c4e8347bcabf74161adadab9b89fe44212b838a5
Leon Schumacher <leonsch@protonmail.com>
2024-02-20 14:09:18 +0100

    template: use the standard input configuration

--- template/flake.nix
+++ template/flake.nix
@@ -1,4 +1,14 @@
 {
-  inputs.aquaris.url = "github:42loco42/aquaris";
+  inputs = {
+    aquaris.url = "github:42loco42/aquaris";
+    aquaris.inputs.home-manager.follows = "home-manager";
+    aquaris.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
   outputs = { aquaris, ... }: aquaris.lib.setup;
 }
