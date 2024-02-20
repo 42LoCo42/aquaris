922c121eb3b130bc575d3667d4e38edff99e4bc3
Leon Schumacher <leonsch@protonmail.com>
2024-02-20 11:54:37 +0100

    [WIP] Switch to unified entrypoint

--- flake.nix
+++ flake.nix
@@ -10,64 +10,92 @@
 
   outputs = { self, nixpkgs, ... }@inputs:
     let
-      nixosModules = import ./lib/nixosModules.nix { inherit self nixpkgs; };
-      lib = {
-        secretsHelper = import ./lib/secretsHelper.nix;
-        aquarisSystems = import ./lib/aquarisSystems.nix
-          { aquaris = self; inherit inputs nixosModules; };
-      };
+      nixosModules = import ./lib/modules.nix { inherit self nixpkgs; };
+      lib = import ./lib/entrypoint.nix { aquaris = self; inherit inputs nixosModules; };
     in
     {
       inherit nixosModules lib;
-      aqscfg = import ./lib/aqs.nix nixpkgs (import self);
+      # aqscfg = import ./lib/aqs.nix nixpkgs (import self);
       templates.default = {
         path = ./template;
         description = "Blank Aquaris config flake";
       };
-    }
-    // lib.aquarisSystems self
-    // inputs.flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; }; in rec {
-        packages = {
-          # castor-installer = pkgs.writeShellApplication {
-          #   name = "castor-installer";
+    } // (
+      let
+        users = {
+          leonsch = {
+            publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJVieLCkWGImVI9c7D0Z0qRxBAKf0eaQWUfMn0uyM/Ql";
+            git = {
+              name = "Leon Schumacher";
+              email = "leonsch@protonmail";
+              key = "C743EE077172986F860FC0FE2F6FE1420970404C";
+            };
+          };
 
-          #   runtimeInputs = with pkgs; [
-          #     git
-          #     nix-output-monitor
-          #     nvd
-          #   ];
-
-          #   text =
-          #     let cfg = self.outputs.nixosConfigurations.castor.config; in
-          #     builtins.readFile (pkgs.substituteAll {
-          #       src = ./lib/combined.sh;
-          #       inherit self;
-          #       name = "castor";
-          #       subs = cfg.nix.settings.substituters;
-          #       keys = cfg.nix.settings.trusted-public-keys;
-          #       masterKeyPath = cfg.aquaris.machine.secretKey;
-          #     });
-          # };
-
-          aqs = pkgs.writeShellApplication {
-            name = "aqs";
-            text = builtins.readFile ./lib/aqs.sh;
-            runtimeInputs = with pkgs; [
-              age
-              jq
-              nix
-            ];
+          guy = {
+            name = "justaguy"; # override actual user name
+            publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF7FppRoKRh+rTSnxFHodYmZ6lVEa4UWN7c0Sgy+trgl";
+            git = {
+              name = "J. A. Guy";
+              email = "guy@example.org";
+              # key is optional
+            };
           };
         };
 
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; with packages; [
-            age
-            aqs
-            nix-output-monitor
-            shfmt
-          ];
+        machines = {
+          castor = {
+            id = "10ec6fa7b2fdeea772a40b31658fead8";
+            publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH8ebcoYSzs9koGhq9KtIqwgcJYj0siYdYv6hVUT/S/G";
+            admins = { inherit (users) leonsch; }; # admins get sudo permissions & encryption access to machine secrets
+            users = { inherit (users) guy; };
+          };
+
+          pollux = {
+            # system = "aarch64-linux"; # optional, x86_64-linux is default
+            id = "e755aeadc6c3f08afd03cf71658c2190";
+            publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGosXDmm4fVV28nlworyjrUxNLzqMVDqbkXGipM7ls+B";
+            admins = { inherit (users) guy; };
+          };
         };
-      });
+      in
+      lib.main self { inherit users machines; }
+      # lib.setup # TODO implement setup
+    );
+
+  # TODO remove dead code
+  # // inputs.flake-utils.lib.eachDefaultSystem (system:
+  #   let pkgs = import nixpkgs { inherit system; }; in rec {
+  #     packages = {
+  #       castor-installer = pkgs.writeShellApplication {
+  #         name = "castor-installer";
+
+  #         runtimeInputs = with pkgs; [
+  #           git
+  #           nix-output-monitor
+  #           nvd
+  #         ];
+
+  #         text =
+  #           let cfg = self.outputs.nixosConfigurations.castor.config; in
+  #           builtins.readFile (pkgs.substituteAll {
+  #             src = ./lib/combined.sh;
+  #             inherit self;
+  #             name = "castor";
+  #             subs = cfg.nix.settings.substituters;
+  #             keys = cfg.nix.settings.trusted-public-keys;
+  #             masterKeyPath = cfg.aquaris.machine.secretKey;
+  #           });
+  #       };
+  #     };
+
+  #     devShells.default = pkgs.mkShell {
+  #       packages = with pkgs; with packages; [
+  #         age
+  #         aqs
+  #         nix-output-monitor
+  #         shfmt
+  #       ];
+  #     };
+  #   });
 }
---
+++ lib/entrypoint.nix
@@ -0,0 +1,102 @@
+{ aquaris, inputs, nixosModules }:
+let
+  inherit (inputs) nixpkgs;
+  inherit (nixpkgs.lib) nixosSystem pipe;
+
+  inherit (import ./utils.nix inputs) my-utils;
+
+  wrap = f: inputs.flake-utils.lib.eachDefaultSystem
+    (system: f (import nixpkgs { inherit system; }));
+
+  aqs = pkgs: pkgs.writeShellApplication {
+    name = "aqs";
+    text = builtins.readFile ./aqs.sh;
+    runtimeInputs = with pkgs; [
+      age
+      jq
+      nix
+    ];
+  };
+
+  setup = wrap (pkgs: {
+    packages = {
+      aqs = aqs pkgs;
+      default = pkgs.writeShellApplication {
+        name = "aquaris-setup";
+        text = "";
+      };
+    };
+  });
+
+  main = self: aqscfg:
+    wrap (pkgs: { packages.aqs = aqs pkgs; }) //
+    {
+      inherit aqscfg;
+      nixosConfigurations = builtins.mapAttrs
+        (name: cfg:
+          let system = cfg.system or "x86_64-linux"; in nixosSystem {
+            inherit system;
+
+            specialArgs = inputs // {
+              inherit
+                aquaris
+                my-utils
+                name
+                self
+                system;
+            };
+
+            modules = builtins.attrValues nixosModules ++ (
+              let d = "${self}/machines/${name}"; in
+              if !builtins.pathExists d then [ ] else
+              pipe d [
+                builtins.readDir
+                builtins.attrNames
+                (map (i: import "${d}/${i}"))
+              ]
+            ) ++ [{
+              aquaris = {
+                # merge admins and users
+                users = builtins.mapAttrs (_: u: u // { isAdmin = true; })
+                  (cfg.admins or { }) // (cfg.users or { });
+
+                machine = {
+                  inherit name;
+                  inherit (cfg) id publicKey;
+                };
+              };
+            }];
+          })
+        aqscfg.machines;
+    };
+in
+{ inherit setup main; }
+
+# TODO remove dead code
+#       installer = pkgs: pkgs.writeShellApplication {
+#         name = "${name}-installer";
+#         runtimeInputs = with pkgs; [
+#           git
+#           gptfdisk
+#           jq
+#           nix-output-monitor
+#         ];
+#         text = subsT ./installer.sh {
+#           inherit name self;
+#           keypath = nixosConfig.config.aquaris.machine.secretKey;
+#           keys = nixosConfig.config.nix.settings.trusted-public-keys;
+#           subs = nixosConfig.config.nix.settings.substituters;
+#         };
+#       };
+
+#       deployer = pkgs: pkgs.writeShellApplication {
+#         name = "${name}-deployer";
+#         runtimeInputs = with pkgs; [
+#           git
+#           openssh
+#         ];
+#         text = subsT ./deployer.sh {
+#           inherit name;
+#           installer = getExe (installer pkgs);
+#         };
+#       };
