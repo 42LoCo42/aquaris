af0c4a88b9f1e1ec52652aaf9bd0a6d2531bf24c
Leon Schumacher <leonsch@protonmail.com>
2024-03-22 09:21:21 +0100

    template & setup: clean up inputs

--- lib/setup/template.nix
+++ lib/setup/template.nix
@@ -3,11 +3,14 @@
     aquaris.url = "github:42loco42/aquaris";
     aquaris.inputs.home-manager.follows = "home-manager";
     aquaris.inputs.nixpkgs.follows = "nixpkgs";
+    aquaris.inputs.obscura.follows = "obscura";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    obscura.url = "github:42loco42/obscura";
   };
 
   outputs = { self, aquaris, ... }:
--- template/flake.nix
+++ template/flake.nix
@@ -1,16 +1,11 @@
 {
   inputs = {
     aquaris.url = "github:42loco42/aquaris";
-    aquaris.inputs.home-manager.follows = "home-manager";
+    aquaris.inputs.home-manager.follows = "";
     aquaris.inputs.nixpkgs.follows = "nixpkgs";
-    aquaris.inputs.obscura.follows = "obscura";
-
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    aquaris.inputs.obscura.follows = "";
 
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
-    obscura.url = "github:42loco42/obscura";
   };
 
   outputs = { aquaris, ... }: aquaris.lib.setup;
