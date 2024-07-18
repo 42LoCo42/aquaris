6ffb72c77a5d81b76d19452b459bf9707af58460
Leon Schumacher <leonsch@protonmail.com>
2024-07-18 20:58:36 +0200

    module/persist: support default user dirs

--- module/persist.nix
+++ module/persist.nix
@@ -6,6 +6,7 @@ let
     mkIf
     mkOption
     pipe
+    unique
     ;
   inherit (lib.types)
     bool
@@ -44,6 +45,10 @@ let
       };
     };
   };
+
+  allParents = file:
+    if file == "/" || file == "." then [ ]
+    else allParents (dirOf file) ++ [ file ];
 in
 {
   options.aquaris.persist = {
@@ -63,27 +68,37 @@ in
       description = "List of persistent directories";
       type = listOf (coercedTo path (d: { inherit d; }) entry);
     };
+
+    userDirs = mkOption {
+      description = "Default list of persistent user directories";
+      type = listOf str;
+    };
   };
 
   config = mkIf cfg.enable {
-    aquaris.persist.dirs = [
-      "/var/lib/nixos"
-      "/var/lib/systemd"
-      "/var/log"
-    ] ++ ifEnable config.networking.networkmanager.enable [
-      { d = "/etc/NetworkManager/system-connections"; m = "0700"; }
-      { d = "/var/lib/NetworkManager"; m = "0755"; }
-    ] ++ ifEnable config.security.sudo.enable [
-      { d = "/var/db/sudo"; m = "0711"; }
-    ] ++ ifEnable config.services.caddy.enable [
-      "/var/lib/caddy"
-    ] ++ ifEnable config.services.tailscale.enable [
-      { d = "/var/lib/tailscale"; m = "0700"; }
-    ] ++ ifEnable config.virtualisation.libvirtd.enable [
-      "/var/lib/libvirt"
-    ] ++ ifEnable config.virtualisation.podman.enable [
-      "/var/lib/containers"
-    ];
+    aquaris.persist = {
+      dirs = [
+        "/var/lib/nixos"
+        "/var/lib/systemd"
+        "/var/log"
+      ] ++ ifEnable config.networking.networkmanager.enable [
+        { d = "/etc/NetworkManager/system-connections"; m = "0700"; }
+        { d = "/var/lib/NetworkManager"; m = "0755"; }
+      ] ++ ifEnable config.security.sudo.enable [
+        { d = "/var/db/sudo"; m = "0711"; }
+      ] ++ ifEnable config.services.caddy.enable [
+        "/var/lib/caddy"
+      ] ++ ifEnable config.services.tailscale.enable [
+        { d = "/var/lib/tailscale"; m = "0700"; }
+      ] ++ ifEnable config.virtualisation.libvirtd.enable [
+        "/var/lib/libvirt"
+      ] ++ ifEnable config.virtualisation.podman.enable [
+        "/var/lib/containers"
+      ];
+
+      userDirs =
+        ifEnable config.programs.zsh.enable (allParents ".cache/zsh");
+    };
 
     fileSystems = pipe cfg.dirs [
       (map (x: {
@@ -111,6 +126,7 @@ in
       (mapAttrsToList (n: x:
         let u = config.users.users.${n}; in
         pipe x.persist [
+          (x: unique (x ++ cfg.userDirs))
           (map (y: [
             "d ${cfg.root}/${u.home}/${y} 0755 ${n} ${u.group} - -"
             "L ${u.home}/${y} - - - - ${cfg.root}/${u.home}/${y}"
