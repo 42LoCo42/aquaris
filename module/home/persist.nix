a5937f6e600cfa96a0b0589f4c3e0379701de5c0
Leon Schumacher <leonsch@protonmail.com>
2025-01-03 11:40:22 +0100

    module/persist: convert dirs to attrset & add enable option

--- module/home/persist.nix
+++ module/home/persist.nix
@@ -1,7 +1,15 @@
 { config, lib, osConfig, ... }:
 let
-  inherit (lib) ifEnable mkOption pipe;
-  inherit (lib.types) coercedTo listOf str submodule;
+  inherit (lib)
+    filterAttrs
+    mapAttrsToList
+    mkIf
+    mkMerge
+    mkOption
+    pipe
+    unique
+    ;
+  inherit (lib.types) attrsOf bool str submodule;
 
   cfg = config.aquaris.persist;
 
@@ -14,20 +22,20 @@ let
 
   mkIn = pfx: map (x: "d ${pfx}/${x} 0755 - - - -");
 
-  mkEntry = x:
+  mkEntry = d: x:
     let
-      persistDirs = pipe x.d [
+      persistDirs = pipe d [
         allParents
         (mkIn "${root}/${home}")
       ];
 
-      targetDirs = pipe x.d [
+      targetDirs = pipe d [
         dirOf
         allParents
         (mkIn home)
       ];
 
-      final = let hd = "${home}/${x.d}"; in [
+      final = let hd = "${home}/${d}"; in [
         "z ${root}/${hd} ${x.m} - - - - "
         "L+ ${hd} - - - - ${root}/${hd}"
       ];
@@ -36,9 +44,10 @@ let
 
   entry = submodule {
     options = {
-      d = mkOption {
-        description = "Directory";
-        type = str;
+      e = mkOption {
+        description = "Enable";
+        type = bool;
+        default = true;
       };
 
       m = mkOption {
@@ -52,27 +61,45 @@ in
 {
   options.aquaris.persist = mkOption {
     description = "List of persistent directories";
-    type = listOf (coercedTo str (d: { inherit d; }) entry);
+    type = attrsOf entry;
   };
 
-  config = ifEnable osConfig.aquaris.persist.enable {
-    aquaris.persist = [
-      ".cache/nix"
-    ] ++ ifEnable config.programs.direnv.enable [
-      ".local/share/direnv"
-    ] ++ ifEnable config.programs.firefox.enable [
-      ".cache/mozilla/firefox"
-      (if config.aquaris.firefox.cleanHome
-      then ".local/share/mozilla/firefox"
-      else ".mozilla/firefox")
-    ] ++ ifEnable config.programs.gpg.enable [
-      { d = ".gnupg"; m = "0700"; }
-    ] ++ ifEnable config.programs.zoxide.enable [
-      ".local/share/zoxide"
-    ] ++ ifEnable config.programs.zsh.enable [
-      ".cache/zsh"
+  config = mkIf osConfig.aquaris.persist.enable {
+    aquaris.persist = mkMerge [
+      {
+        ".cache/nix" = { };
+      }
+
+      (mkIf config.programs.direnv.enable {
+        ".local/share/direnv" = { };
+      })
+
+      (mkIf config.programs.firefox.enable {
+        ".cache/mozilla/firefox" = { };
+
+        ${if config.aquaris.firefox.cleanHome
+        then ".local/share/mozilla/firefox"
+        else ".mozilla/firefox"} = { };
+      })
+
+      (mkIf config.programs.gpg.enable {
+        ".gnupg" = { m = "0700"; };
+      })
+
+      (mkIf config.programs.zoxide.enable {
+        ".local/share/zoxide" = { };
+      })
+
+      (mkIf config.programs.zsh.enable {
+        ".cache/zsh" = { };
+      })
     ];
 
-    systemd.user.tmpfiles.rules = builtins.concatMap mkEntry cfg;
+    systemd.user.tmpfiles.rules = pipe cfg [
+      (filterAttrs (_: x: x.e))
+      (mapAttrsToList mkEntry)
+      builtins.concatLists
+      unique
+    ];
   };
 }
--- module/persist.nix
+++ module/persist.nix
@@ -1,15 +1,16 @@
 { config, lib, ... }:
 let
-  inherit (lib) ifEnable mapAttrsToList mkIf mkOption pipe;
-  inherit (lib.types) bool coercedTo listOf path str submodule;
+  inherit (lib) filterAttrs mapAttrsToList mkIf mkMerge mkOption pipe;
+  inherit (lib.types) attrsOf bool path str submodule;
 
   cfg = config.aquaris.persist;
 
   entry = submodule {
     options = {
-      d = mkOption {
-        description = "Directory";
-        type = path;
+      e = mkOption {
+        description = "Enable";
+        type = bool;
+        default = true;
       };
 
       m = mkOption {
@@ -48,45 +49,65 @@ in
 
     dirs = mkOption {
       description = "List of persistent directories";
-      type = listOf (coercedTo path (d: { inherit d; }) entry);
+      type = attrsOf entry;
+    };
+
+    dirs' = mkOption {
+      description = "List of ENABLED persistent directories";
+      type = attrsOf entry;
     };
   };
 
   config = mkIf cfg.enable {
     aquaris.persist = {
-      dirs = [
-        "/var/lib/systemd"
-        "/var/log"
-      ] ++ ifEnable config.boot.lanzaboote.enable [
-        config.boot.lanzaboote.pkiBundle
-      ] ++ ifEnable config.networking.networkmanager.enable [
-        { d = "/etc/NetworkManager/system-connections"; m = "0700"; }
-        { d = "/var/lib/NetworkManager"; m = "0755"; }
-      ] ++ ifEnable config.security.sudo.enable [
-        { d = "/var/db/sudo"; m = "0711"; }
-      ] ++ ifEnable config.services.caddy.enable [
-        "/var/lib/caddy"
-      ] ++ ifEnable config.services.tailscale.enable [
-        { d = "/var/lib/tailscale"; m = "0700"; }
-      ] ++ ifEnable config.virtualisation.libvirtd.enable [
-        "/var/lib/libvirt"
-      ] ++ ifEnable config.virtualisation.podman.enable [
-        "/var/lib/containers"
+      dirs = mkMerge [
+        {
+          "/var/lib/systemd" = { };
+          "/var/log" = { };
+        }
+
+        (mkIf config.boot.lanzaboote.enable {
+          ${config.boot.lanzaboote.pkiBundle} = { };
+        })
+
+        (mkIf config.networking.networkmanager.enable {
+          "/etc/NetworkManager/system-connections" = { m = "0700"; };
+          "/var/lib/NetworkManager" = { m = "0755"; };
+        })
+
+        (mkIf config.security.sudo.enable {
+          "/var/db/sudo" = { m = "0711"; };
+        })
+
+        (mkIf config.services.caddy.enable {
+          "/var/lib/caddy" = { };
+        })
+
+        (mkIf config.services.tailscale.enable {
+          "/var/lib/tailscale" = { m = "0700"; };
+        })
+
+        (mkIf config.virtualisation.libvirtd.enable {
+          "/var/lib/libvirt" = { };
+        })
+
+        (mkIf config.virtualisation.podman.enable {
+          "/var/lib/containers" = { };
+        })
       ];
+
+      dirs' = filterAttrs (_: x: x.e) cfg.dirs;
     };
 
-    fileSystems = pipe cfg.dirs [
-      (map (x: {
-        name = x.d;
-        value = {
-          device = "${cfg.root}/${x.d}";
-          options = [
-            "bind"
-            "x-aquaris.persist=${x.m}"
-          ];
-        };
+    fileSystems = pipe cfg.dirs' [
+      (builtins.mapAttrs (d: x: {
+        device = "${cfg.root}/${d}";
+        options = [
+          "bind"
+          "x-aquaris.persist=${x.m}"
+        ];
       }))
-      builtins.listToAttrs
+
       (x: x // {
         ${cfg.root}.neededForBoot = true;
 
@@ -99,9 +120,9 @@ in
 
     systemd.tmpfiles.rules =
       let
-        system = map
-          (x: "d ${cfg.root}/${x.d} ${x.m} ${x.u} ${x.g} - -")
-          cfg.dirs;
+        system = mapAttrsToList
+          (d: x: "d ${cfg.root}/${d} ${x.m} ${x.u} ${x.g} - -")
+          cfg.dirs';
 
         homes = pipe config.aquaris.users [
           (mapAttrsToList (n: _: config.users.users.${n}))
